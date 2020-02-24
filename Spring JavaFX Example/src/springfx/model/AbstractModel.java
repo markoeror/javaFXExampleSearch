9583ee66a024e37a3bbfa531ecc199ac66d67ecf
zarko <zarko.trampic@web-ing.rs>
2020-02-24 12:28:50 +0100

    start

---
+++ Spring JavaFX Example/src/springfx/model/AbstractModel.java
@@ -0,0 +1,26 @@
+package springfx.model;
+
+public abstract class AbstractModel implements Model {
+
+    @Override
+    public final Contact getCurrentContact() {
+
+        return currentContactProperty().get();
+    }
+    
+    @Override
+    public final void setCurrentContact(Contact contact) {
+
+        currentContactProperty().set(contact);
+    }
+    
+    @Override
+    public final String getSearchFilter() {
+        return searchFilterProperty().get();
+    }
+    
+    @Override
+    public final void setSearchFilter(String searchFilter) {
+        searchFilterProperty().set(searchFilter);
+    }
+}
